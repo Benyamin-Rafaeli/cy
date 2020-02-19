8f80f65f101842cba9a9adbc73991a9545e874b5
benyamin <benyamin.rafaeli@gmail.com>
2020-02-19 15:02:15 +0200

    WIP 10s delay

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh 
 npm run srv &
-sleep 5s
+sleep 10s
 npx cypress run
 npx mochawesome-merge cypress/reports/mocha/*.json > cypress/mochareports/report.json
 npx marge cypress/mochareports/*.json -f report -o cypress/mochareports
